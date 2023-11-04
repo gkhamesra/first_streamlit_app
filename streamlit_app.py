247b2ee128f60c65919c284c7ff7b43e46b52c67
gkhamesra <gkhamesra@users.noreply.github.com>
2023-11-03 22:49:06 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json()) # outputs the response as is
 
 # normalizes the json
-fruityvice_normalized = pandas.json_normalize(fruitvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # creates a table out of the normalized json
 streamlit.dataframe(fruityvice_normalized)
