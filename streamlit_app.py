6b2daea6d3f3e753d817e4d43eb6c602e7a0780c
gkhamesra <gkhamesra@users.noreply.github.com>
2023-11-05 09:18:19 -0500

    Update streamlit_app.py
    created custom function

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,15 +30,20 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
 
+# adding custom function
+def fruityvice_fruit_choice(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information')
   else:
-    streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    #streamlit.write('The user entered ', fruit_choice)
+    back_from_function = fruityvice_fruit_choice(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
